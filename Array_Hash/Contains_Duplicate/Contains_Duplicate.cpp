5296fba9344812f176a7e91e171a9863ffda264d
tzhsuan <59945545+tzhsuan@users.noreply.github.com>
2025-05-02 20:40:32 +0800

    Add files via upload

---
+++ Array_Hash/Contains_Duplicate/Contains_Duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    public:
+        bool hasDuplicate(vector<int>& nums) {
+            unordered_set<int> set;
+            for(int num : nums){
+                if(set.count(num)){
+                    return true;
+                }
+                set.insert(num);
+            }
+            return false;
+        }
+    };
